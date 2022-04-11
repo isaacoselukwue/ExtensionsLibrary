55336cfd73a6e078ecbba4e03ba859fc1f6fa319
Isaac Oselukwue <princeizak@live.com>
2022-04-11 17:33:56 +0100

    Guid Validator for blocking null or zero guids.

---
+++ ExtensionsLibrary/Utilities/GuidValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionsLibrary.Utilities
+{
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Parameter, AllowMultiple = false)]
+    public class GuidValidator : ValidationAttribute
+    {
+        public GuidValidator():base("The {0} field requires a non-default value.")
+        {
+
+        }
+        public override bool IsValid(object? value)
+        {
+            if (value is null)
+            {
+                return false;
+            }
+            var type = value.GetType();
+            return !Equals(value, Activator.CreateInstance(Nullable.GetUnderlyingType(type) ?? type));
+        }
+    }
+}
