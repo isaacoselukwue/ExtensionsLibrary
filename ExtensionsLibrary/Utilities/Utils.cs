40975166fb46efbc8ed7ac2696bbfd5292e0786f
Isaac Oselukwue <Isaac.Oselukwue@sterling.ng>
2022-12-14 18:38:02 +0100

    Update to add a roburst getdate policy

---
+++ ExtensionsLibrary/Utilities/Utils.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionsLibrary.Utilities
+{
+    internal class Utils
+    {
+        public static DateTime GetDateFromString(string dateString)
+        {
+			try
+			{
+                CultureInfo provider = CultureInfo.InvariantCulture;
+                string[] format = new string[] { "dd/MM/yyyy h:mm", "dd-MM-yyyy h:mm", "dd/MM/yyyy HH:mm", "dd/MM/yyyy", "dd-MM-yyyy HH:mm", "dd-MM-yyyy", "M/dd/yyyy hh:mm", "MM/dd/yyyy hh:mm", "MM/dd/yyyy hh:mm:ss", "d/MM/yyyy h:mm:ss tt", "MM/dd/yyyy hh:mm:ss tt" };
+                DateTime.TryParseExact(dateString, format, provider, DateTimeStyles.None, out DateTime date);
+                return date;
+            }
+			catch (Exception)
+			{
+
+				throw;
+			}
+        }
+    }
+}
